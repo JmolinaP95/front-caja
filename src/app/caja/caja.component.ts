32c91c76587f8361c78fa1864b80948d158010dd
JmolinaP95 <jesus.molina@fbim-consultor.com>
2023-12-06 16:36:22 -0500

    commit jm

--- src/app/caja/caja.component.ts
+++ src/app/caja/caja.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CajaService } from '../service/caja.service';
+import { MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-caja',
   templateUrl: './caja.component.html',
-  styleUrls: ['./caja.component.css']
+  styleUrls: ['./caja.component.css'],
+  providers: [MessageService, ConfirmationService]
 })
 export class CajaComponent implements OnInit {
   data: any [] = [];
@@ -23,6 +26,19 @@ export class CajaComponent implements OnInit {
   
   ngOnInit() {
     this.llenarData();
+
+    this.cols = [
+      { field: 'id_caja', header: 'ID Caja xx' },
+      { field: 'fecha', header: 'Fecha' },
+      { field: 'descripcion', header: 'Descripción' },
+      { field: 'estado_registro', header: 'Estado Registro' },
+      { field: 'usuario_ingreso', header: 'Usuario Ingreso' },
+      { field: 'fecha_ingreso', header: 'Fecha Ingreso' },
+      { field: 'ip_ingreso', header: 'IP Ingreso' },
+      { field: 'fecha_modificacion', header: 'Fecha Modificación' },
+      { field: 'usuario_modificacion', header: 'Usuario Modificación' },
+      { field: 'ip_modificacion', header: 'IP Modificación' }
+    ];
   }
 
 
--- src/app/service/caja.service.ts
+++ src/app/service/caja.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,7 +14,15 @@ export class CajaService {
   getAll() : Observable<any>{
     return this.http.get<any>(this.baseUrl);
   }
+  
+  save(caja: any): Observable<any> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json');
+    return this.http.post(this.baseUrl, JSON.stringify(caja), { headers: headers });
+  }
 
-
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
   
 }
